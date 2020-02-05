873f7714a4202537565e9955147cfd5b7ca85044
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-02-05 11:54:30 +0530

    added test file
    installed npm and mocha

--- calc/add.js
+++ calc/add.js
@@ -1,3 +1,12 @@
 const add = function(a, b) {
   return a + b;
 };
+
+const main = function() {
+  const [num1, num2] = process.argv.slice(2);
+  console.log(add(+num1, +num2));
+};
+
+main();
+
+module.exports = add;
