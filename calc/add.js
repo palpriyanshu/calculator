5185b65998239f73123999f21f8db8163a7a02e0
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-02-05 11:49:56 +0530

    merge conflict

--- calc/add.js
+++ calc/add.js
@@ -4,10 +4,6 @@ const add = function(a, b) {
   return c + d;
 };
 
-<<<<<<< HEAD
 const sub = (a, b) => {
-=======
-const sub = function(a, b) {
->>>>>>> 1c4db3e6282dd92a30b7e64a21a399e3a6f7f05a
   return a - b;
 };
