b9276d68eec88b26a4f056f9e16e2473cc5e3b4f
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-02-05 12:02:40 +0530

    added test for sub. rename addTest to calcTest

--- test/addTest.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const add = require('../calc/add.js');
-
-describe('add', function() {
-  it('should add two num', function() {
-    assert.strictEqual(add(2, 3), 5);
-  });
-});
