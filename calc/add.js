9ff4a0437f14555cbdc445de4063fbb3fdb9c8b1
ayush <58028488+desibabua@users.noreply.github.com>
2020-02-05 12:05:15 +0530

    modified main function

--- calc/add.js
+++ calc/add.js
@@ -6,11 +6,14 @@ const sub = function(firstNo, secondNo) {
   return firstNo - secondNo;
 };
 
-const main = function() {
-  const [num1, num2] = process.argv.slice(2);
-  console.log(add(+num1, +num2));
+const main = function(num1, num2, fn) {
+  if (fn == 'sum' || fn == 'add') {
+    console.log(add(+num1, +num2));
+    return;
+  }
+  console.log(sub(+num1, +num2));
 };
 
-main();
+main(...process.argv.slice(2));
 
 module.exports = add;
