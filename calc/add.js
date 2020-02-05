3456c10539c1e49536fc02f2c12369336e75bb38
gulshan kumar <gulhan@123>
2020-02-05 11:52:53 +0530

    refacored code in calc.js

--- calc/add.js
+++ calc/add.js
@@ -1,13 +1,7 @@
-const add = function(a, b) {
-  c = a;
-  d = b;
-  return c + d;
+const add = function(firstNo, secondNo) {
+  return firstNo + secondNo;
 };
 
-<<<<<<< HEAD
-const sub = (a, b) => {
-=======
-const sub = function(a, b) {
->>>>>>> 1c4db3e6282dd92a30b7e64a21a399e3a6f7f05a
-  return a - b;
+const sub = function(firstNo, secondNo) {
+  return firstNo - secondNo;
 };
